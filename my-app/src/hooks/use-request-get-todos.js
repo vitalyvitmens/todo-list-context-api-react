32a39ec97a876324f40fa52278632d2dc85e3998
vitalyvitmens <otm_@tut.by>
2023-07-18 10:37:28 +0300

    add data in project

--- my-app/src/hooks/use-request-get-todos.js
+++ my-app/src/hooks/use-request-get-todos.js
@@ -17,28 +17,3 @@ export const useRequestGetTodos = (refreshTodos, setTodosServer) => {
 		isLoading: isLoading,
 	}
 }
-
-// export const useRequestGetTodos = (refreshTodos, setTodosServer, sortTitle) => {
-// 	const [isLoading, setIsLoading] = useState(false)
-
-// 	useEffect(() => {
-// 		setIsLoading(true)
-// 		sortTitle
-// 			? fetch('http://localhost:8204/todos?_sort=title')
-// 					.then((loadedData) => loadedData.json())
-// 					.then((loadedTodo) => {
-// 						setTodosServer(loadedTodo)
-// 					})
-// 					.finally(() => setIsLoading(false))
-// 			: fetch('http://localhost:8204/todos')
-// 					.then((loadedData) => loadedData.json())
-// 					.then((loadedTodo) => {
-// 						setTodosServer(loadedTodo)
-// 					})
-// 					.finally(() => setIsLoading(false))
-// 	}, [refreshTodos, sortTitle, setTodosServer])
-
-// 	return {
-// 		isLoading: isLoading,
-// 	}
-// }
